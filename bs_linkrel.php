a182d04d1cd86777da824dbe3fc2f46e4a4cb187
jafrax <jafraxx@yahoo.co.id>
2017-07-13 00:41:40 +0700

    repositori

---
+++ bs_linkrel.php
@@ -0,0 +1,35 @@
+<!-- Le styles -->
+    <link href="<?php echo $addr_root_css;?>bootstrap.css" rel="stylesheet">
+<style>
+      body {
+        padding-top: 0px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+    <link href="<?php echo $addr_root_css;?>bootstrap-responsive.css" rel="stylesheet">
+    <!--
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">    
+    -->
+    <link href="<?php echo $addr_root_css;?>custom.css" rel="stylesheet">
+    
+    <link href="<?php echo $addr_root_spry;?>SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+    
+    <!-- SPRY -->
+    <!--
+    <script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+    <script src="SpryAssets/SpryValidationTextarea.js" type="text/javascript"></script>
+    <script src="SpryAssets/SpryValidationSelect.js" type="text/javascript"></script>
+    <script src="SpryAssets/SpryValidationCheckbox.js" type="text/javascript"></script>
+    <script src="SpryAssets/SpryValidationRadio.js" type="text/javascript"></script>
+    <link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+    <link href="SpryAssets/SpryValidationTextarea.css" rel="stylesheet" type="text/css" />
+    <link href="SpryAssets/SpryValidationSelect.css" rel="stylesheet" type="text/css" />
+    <link href="SpryAssets/SpryValidationCheckbox.css" rel="stylesheet" type="text/css" />
+    <link href="SpryAssets/SpryValidationRadio.css" rel="stylesheet" type="text/css" />
+    -->
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo $addr_root_css;?>datepicker/jquery.ui.all.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo $addr_root_css;?>jquery-ui-1.8.10.custom.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo $addr_root_validation_engine;?>validationEngine.jquery.css" />
+   
---
+++ script_awal.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+// error_reporting(1);
+// error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+
+
+$doc_root = $_SERVER['DOCUMENT_ROOT'].'/gudang/';
+$doc_root_class = $_SERVER['DOCUMENT_ROOT'].'/gudang/class/';
+$doc_root_js = $_SERVER['DOCUMENT_ROOT'].'/gudang/asset/js/';
+$doc_root_css = $_SERVER['DOCUMENT_ROOT'].'/gudang/asset/css/';
+$doc_root_pics = $_SERVER['DOCUMENT_ROOT'].'/gudang/asset/pics/';
+$doc_root_spry = $_SERVER['DOCUMENT_ROOT'].'/gudang/asset/SpryAssets/';
+$doc_root_validation_engine = $_SERVER['DOCUMENT_ROOT'].'/gudang/asset/validation_engine/';
+
+$addr_root = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/';
+$addr_root_class = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/class/';
+$addr_root_js = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/asset/js/';
+$addr_root_css = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/asset/css/';
+$addr_root_pics = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/asset/pics/';
+$addr_root_spry = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/asset/SpryAssets/';
+$addr_root_validation_engine = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/asset/validation_engine/';
+
+$addr_server = 'http://'.$_SERVER['HTTP_HOST'].'/gudang/';
+$server_name = $_SERVER['HTTP_HOST'].'/gudang/';
+
+
+if($_SERVER['HTTP_REFERER']==''){
+    $_SESSION['warning'] = 'Silahkan login untuk mengakses halaman ini!';
+    header("Location: ".$addr_root."index.php");
+    exit();
+}
+
+if(!isset($_SESSION['username'])){
+    $_SESSION['warning'] = 'Silahkan login!';
+    header("Location: ".$addr_root."index.php");
+    exit();
+}
+
+// $inactive = 6;
+// echo $sessionTTL = time() - $_SESSION['timeout'];
+// if ($sessionTTL > $inactive) {
+//     header("Location: logout.php");
+// }
+
+?>
